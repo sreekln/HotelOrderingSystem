1a4805b076a5a1be3f39d2e5780bf2cd8feace9e
sreekln <sreenavya.kln@gmail.com>
2025-09-13 09:23:15 +0530

    Updated CustomerDashboard.tsx

--- src/components/CustomerDashboard.tsx
+++ src/components/CustomerDashboard.tsx
@@ -79,7 +79,7 @@ const CustomerDashboard: React.FC = () => {
   };
 
   const getTotalAmount = () => {
-    return cart.reduce((total, cartItem) => total + (cartItem.item.price * cartItem.quantity), 0);
+    return cart.reduce((total, cartItem) => total + (cartItem.item.price * cartItem.quantity * cartItem.item.tax_rate / 100), 0);
   };
 
   const getSubtotal = () => {
@@ -91,7 +91,7 @@ const CustomerDashboard: React.FC = () => {
   };
 
   const getFinalTotal = () => {
-    return calculateTotal(getSubtotal() + getTaxAmount());
+    return calculateTotal(getSubtotal());
   };
 
   const createOrder = async () => {
